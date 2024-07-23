71aeb1d76ad0144c91757179ff60b6576a62ef3c
Yordiemma <yordisweet56@gmail.com>
2024-07-23 03:06:45 +0200

    Initial commit

---
+++ blogr-landing-page-main/tailwind.config.js
@@ -0,0 +1,12 @@
+// tailwind.config.js
+module.exports = {
+  content: ['./*.html'],
+  theme: {
+    extend: {
+      colors: {
+        'dark-blue': '#2D314D', // or the specific color code you need
+      },
+    },
+  },
+  plugins: [],
+}
