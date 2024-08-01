eaec26bceb62ab7e86fd0b9db24a70b02618259e
Yordiemma <yordisweet56@gmail.com>
2024-08-02 00:58:02 +0200

    Update API routes

---
+++ Laraval-Api/app/Http/Controllers/ProductController.php
@@ -0,0 +1,107 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Product;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function index()
+    {
+        $products = Product::all();
+        return response()->json($products, 200);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required',
+            'slug' => 'required|unique:products,slug',
+            'price' => 'required|numeric'
+        ]);
+
+        $product = Product::create($request->all());
+
+        return response()->json($product, 201);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function show($id)
+    {
+        $product = Product::find($id);
+
+        if (is_null($product)) {
+            return response()->json(['message' => 'Product not found'], 404);
+        }
+
+        return response()->json($product, 200);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function update(Request $request, $id)
+    {
+        $product = Product::find($id);
+
+        if (is_null($product)) {
+            return response()->json(['message' => 'Product not found'], 404);
+        }
+
+        $product->update($request->all());
+
+        return response()->json($product, 200);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function destroy($id)
+    {
+        $product = Product::find($id);
+
+        if (is_null($product)) {
+            return response()->json(['message' => 'Product not found'], 404);
+        }
+
+        $product->delete();
+
+        return response()->json(null, 204);
+    }
+
+    /**
+     * Search for a name
+     *
+     * @param  string  $name
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function search($name)
+    {
+        $products = Product::where('name', 'like', '%'.$name.'%')->get();
+        return response()->json($products, 200);
+    }
+}
