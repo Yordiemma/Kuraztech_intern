4a7d2e95da436ba604003cdec3a209e899b734f7
Yordiemma <yordisweet56@gmail.com>
2024-08-04 23:50:29 +0200

    Add construction_management directory

---
+++ construction_management/resources/views/welcome.blade.php
@@ -0,0 +1,19 @@
+<!-- resources/views/welcome.blade.php -->
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Welcome To MUHIRA Construction</title>
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+    <div class="container mt-5">
+        <div class="jumbotron text-center">
+            <h1 class="display-4">Welcome To MUHIRA Construction</h1>
+            <p class="lead">We build your dreams.</p>
+        </div>
+    </div>
+</body>
+</html>
